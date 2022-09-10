4418ed9d38c682273cb46305c6bc03f7c6bbe159
Takatoshi-Miura <takatoshi052031@gmail.com>
2022-09-10 17:53:45 +0900

    [Add] ホーム画面にUI追加

--- TaskRanker/Controller/Home/HomeViewController.swift
+++ TaskRanker/Controller/Home/HomeViewController.swift
@@ -8,22 +8,57 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    // MARK: - UI,Variable
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var taskListView: UIView!
+    @IBOutlet weak var completedTaskButton: UIButton!
+    private var isFilter: Bool = false
 
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    /// 画面初期化
+    private func initView() {
+        self.title = ""
+        
+        // 設定メニュー
+        let isDarkMode = UITraitCollection.current.userInterfaceStyle == .dark
+        let menuImage = isDarkMode ? UIImage(named: "humburger_menu_white")! : UIImage(named: "humburger_menu_black")!
+        let menuButton = UIBarButtonItem(image: menuImage.withRenderingMode(.alwaysOriginal),
+                                         style: .plain,
+                                         target: self,
+                                         action: #selector(openHumburgerMenu(_:)))
+        
+        // フィルタ
+        let filterImage = isFilter ? UIImage(named: "icon_filter_fill")! : UIImage(named: "icon_filter_empty")!
+        let filterButton = UIBarButtonItem(image: filterImage,
+                                           style: .done,
+                                           target: self,
+                                           action: #selector(openFilterMenu))
+        
+        navigationItem.leftBarButtonItems = [menuButton]
+        navigationItem.rightBarButtonItems = [filterButton]
     }
-    */
+    
+    // MARK: - Action
+    
+    /// 設定メニュー
+    @objc func openHumburgerMenu(_ sender: UIBarButtonItem) {
+//        delegate?.taskVCSettingDidTap(self)
+    }
+    
+    /// 設定メニュー
+    @objc func openFilterMenu(_ sender: UIBarButtonItem) {
+//        delegate?.taskVCSettingDidTap(self)
+    }
+    
+    /// タスク追加
+    @IBAction func tapAddButton(_ sender: Any) {
+    }
+    
 
 }

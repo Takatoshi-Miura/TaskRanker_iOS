26ad175dd657499b42df6a61b9c28a7be026033a
Takatoshi-Miura <takatoshi052031@gmail.com>
2022-09-10 16:59:46 +0900

    [Add] 多言語対応用ファイルを追加

---
+++ TaskRanker/Resource/LocalizableString.swift
@@ -0,0 +1,11 @@
+//
+//  LocalizableString.swift
+//  TaskRanker
+//
+//  Created by Takatoshi Miura on 2022/09/10.
+//
+
+import UIKit
+
+// MARK: - Common
+let TITLE_CANCEL = NSLocalizedString("Cancel", comment: "")
